5032441e3fd07f675cf99426683b06b10db47d33
PulidoVPE <pulidovpe.dev@gmail.com>
2025-08-22 10:50:11 -0500

    MOD: Refactorizacion

--- terraform/oidc/main.tf
+++ terraform/oidc/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Crear OIDC Provider si no existe
+# Crear OIDC Provider si no existe (se importará si ya existe)
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
 }
 
 # Crear IAM Role
